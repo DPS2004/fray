a94fae3a6610800687c3402ee67531cd537ae7d5
DPS2004 <10176105+DPS2004@users.noreply.github.com>
2023-01-22 20:20:56 -0500

    ok officially getting annoyed by the lack of stage resources here

--- jellycar_stage/library/scripts/stage/jellycar_stageScript.hx
+++ jellycar_stage/library/scripts/stage/jellycar_stageScript.hx
@@ -1,18 +1,11 @@
 // API Script for Stage Template
 
-var CLOUD_BACK_2_X_SPEED = 0.05; // Cloud back 2 scroll speed
-var CLOUD_BACK_2_WIDTH = 1542; // Determines loop point, should match physical size based on stats scaling
-var CLOUD_BACK_1_X_SPEED = 0.075; // Cloud back 1 scroll speed
-var CLOUD_BACK_1_WIDTH = 1542; // Determines loop point, should match physical size based on stats scaling
 
-// Vars
-var cloudBack2XOffset = self.makeFloat(0);
-var cloudBack1XOffset = self.makeFloat(0);
 
 function initialize(){
 	// Don't animate the stage itself (we'll pause on one version for hazards on, and another version for hazards off)
 	self.pause();
-
+	match.createStructure(self.getResource().getContent("jellycarBorder"));
 	if (match.getMatchSettingsConfig().hazards) {
 		// Hazards are on, enable the platform beneath the stage
 		//match.createStructure(self.getResource().getContent("stagetemplateMovingPlatform"));
